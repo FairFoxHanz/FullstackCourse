2a96565c3545bd1aea8abf37bcf63c576135a90b
FairFoxHanz <fairfoxhanz@gmail.com>
2018-01-03 22:07:24 +0100

    Refactored and added mongoose

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('./services/passport');
+
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
 
-//https://still-crag-86568.herokuapp.com/ 
+mongoose.connect(keys.mongoURI);
 
-app.get('/', (req, res) => {
-    res.send({
-        "HI": "THERE"
-    });
-});
+
+require('./routes/auth_routes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
