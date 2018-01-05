4107562a77ea2e0e58daf233019d9890e051078a
FairFoxHanz <fairfoxhanz@gmail.com>
2018-01-05 22:16:07 +0100

    Finished authentication flow

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,32 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);    
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);    
+    });
+});
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id }).then(existingUser => {
-        if(!existingUser) {
-            new User({ googleId: profile.id }).save();
+        if (existingUser) {
+            done(null, existingUser);
+        } else {
+            new User({ googleId: profile.id }).save().then(newUser => {
+                done(null, newUser);
+            }).catch(error => {
+                done(error, null);
+            });
         }
-    })
+    }).catch(error => {
+        done(error, null);
+    });
 }));
