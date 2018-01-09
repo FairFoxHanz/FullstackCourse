03b98846ef873d5d17163d0ff062336378e3daf8
FairFoxHanz <fairfoxhanz@gmail.com>
2018-01-09 22:45:09 +0100

    Added front and refactored passport

--- services/passport.js
+++ services/passport.js
@@ -6,12 +6,12 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);    
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
     User.findById(id).then(user => {
-        done(null, user);    
+        done(null, user);
     });
 });
 
@@ -20,18 +20,16 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-            done(null, existingUser);
-        } else {
-            new User({ googleId: profile.id }).save().then(newUser => {
-                done(null, newUser);
-            }).catch(error => {
-                done(error, null);
-            });
-        }
-    }).catch(error => {
-        done(error, null);
+}, async(accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({
+        googleId: profile.id
     });
+    if (existingUser) {
+        done(null, existingUser);
+    }
+    const newUser = await new User({
+        googleId: profile.id
+    })
+    newUser.save();
+    done(null, newUser);
 }));
