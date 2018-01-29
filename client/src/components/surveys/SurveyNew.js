aa23bd28238c366a0aadafc5612588dd0b4c0405
FairFoxHanz <fairfoxhanz@gmail.com>
2018-01-29 19:00:11 +0100

    Redux-form set up

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class SurveyNew extends Component {
+  render() {
+    return <div> Survey New </div>;
+  }
+}
+
+export default SurveyNew;
