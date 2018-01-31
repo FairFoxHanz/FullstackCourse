789cecedcfde5247a09bda74074b300b6820814a
FairFoxHanz <fairfoxhanz@gmail.com>
2018-01-31 20:08:28 +0100

    Added fields for adding surveys

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import SurveyForm from "./SurveyForm";
 
 class SurveyNew extends Component {
   render() {
-    return <div> Survey New </div>;
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
   }
 }
 
