ff83c952cc1f0f9f4db2d6c5a3f95b17718a9260
FairFoxHanz <fairfoxhanz@gmail.com>
2018-02-11 21:39:45 +0100

    Added get handler

--- routes/survey_routes.js
+++ routes/survey_routes.js
@@ -10,32 +10,55 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id });
+  
+    res.send(surveys);
+  });
+
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path("/api/surveys/:surveyId/:choice");
-      const match = p.test(pathname);
-      if (match) {
-        return {
-          email,
-          surveyId: match.surveyId,
-          choice: match.choice
-        };
-      }
-    });
+    const p = new Path("/api/surveys/:surveyId/:choice");
 
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
-    console.log(uniqueEvents);
+    _.chain(req.bodymap)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ id, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: id,
+            recipients: {
+              $elemMatch: { email, responden: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: Date.now()
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-    
+
     const survey = new Survey({
       title,
       body,
@@ -52,7 +75,7 @@ module.exports = app => {
       await survey.save();
       req.user.credits -= 1;
       const user = await req.user.save();
-      
+
       res.send(user);
     } catch (error) {
       res.status(422).send(error);
