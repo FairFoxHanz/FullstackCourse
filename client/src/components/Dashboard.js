200658639bbdb0c2f3924f5040d67cdf2b9705be
FairFoxHanz <fairfoxhanz@gmail.com>
2018-02-12 18:46:03 +0100

    Finalized project

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SurveyList from "./surveys/SurveyList";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div>
+      <SurveyList />
       <div className="fixed-action-btn">
         <Link className="btn-floating btn-large red" to="/surveys/new">
           <i className="material-icons">add</i>
